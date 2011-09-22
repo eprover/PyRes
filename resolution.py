862c2ce736b38aecd434498d8a46df229dbdb0d5
Stephan Schulz <schulz@eprover.org>
2011-09-22 22:23:17 +0000

    Added factoring

--- resolution.py
+++ resolution.py
@@ -4,8 +4,11 @@
 # Module resolution.py
 
 """
-This module implements binary resolution, as given by the following
+This module implements the rules of the simple resolution calculus,
+namely binary resolution and factoring.
 inference rule:
+
+Binary resolution:
 
 c1|a1     c2|~a2
 ---------------- where sigma=mgu(a1,a2)
@@ -13,6 +16,18 @@ c1|a1     c2|~a2
 
 Note that c1 and c2 are arbitrary disjunctions of literals, possibly
 empty, and that a1 and a2 are atoms (so a1 and ~a2 are literals).
+Also, since | is AC (or, alternatively, the clauses are unordered
+multisets), the order of literals is irrelevant.
+
+
+
+Factoring:
+
+   c|a|b
+----------  where sigma = mgu(a,b)
+sigma(c|a)
+
+Again, c is an arbitray disjunction.
 
 
 Copyright 2010-2011 Stephan Schulz, schulz@eprover.org
@@ -68,6 +83,27 @@ def resolution(clause1, lit1, clause2, lit2):
     return res
 
 
+def factor(clause, lit1, lit2):
+    """
+    Check if it is possible to form a factor between lit1 and lit2. If
+    yes, return it, otherwise return None.
+    """
+    l1 = clause.getLiteral(lit1)
+    l2 = clause.getLiteral(lit2)
+    if l1.isNegative() != l2.isNegative():
+        return None
+    sigma = mgu(l1.atom, l2.atom)
+    print sigma
+    if sigma == None:
+        return None
+    lits = [l.instantiate(sigma) for l in clause.literals if l!=l2]
+    res = clauses.Clause(lits)
+    return res
+
+
+    
+
+
 class TestResolution(unittest.TestCase):
     """
     Unit test class for clauses. Test clause and literal
@@ -82,11 +118,13 @@ class TestResolution(unittest.TestCase):
 cnf(c1,axiom,p(a, X)|p(X,a)).
 cnf(c2,axiom,~p(a,b)|p(f(Y),a)).
 cnf(c3,axiom,p(Z,X)|~p(f(Z),X0)).
+cnf(c4,axiom,p(X,X)|p(a,f(Y))).
 """
         lex = Lexer(self.spec)
         self.c1 = clauses.parseClause(lex)
         self.c2 = clauses.parseClause(lex)
         self.c3 = clauses.parseClause(lex)
+        self.c4 = clauses.parseClause(lex)
        
     def testResolution(self):
         """
@@ -97,7 +135,7 @@ cnf(c3,axiom,p(Z,X)|~p(f(Z),X0)).
         print res1
 
         res2 = resolution(self.c1, 0, self.c3,0)
-        self.assert_(not res2)
+        self.assert_(res2==None)
         print res2
 
         res3 = resolution(self.c2, 0, self.c3,0)
@@ -105,10 +143,25 @@ cnf(c3,axiom,p(Z,X)|~p(f(Z),X0)).
         print res3
 
         res4 = resolution(self.c1, 0, self.c3,1)
-        self.assert_(not res4)
+        self.assert_(res4==None)
         print res4
 
+    def testFactoring(self):
+        """
+        Test the factoring inference.
+        """        
+        f1 = factor(self.c1,0,1)
+        self.assert_(f1)
+        self.assert_(len(f1)==1)
+        print "Factor:", f1
+        
+        f2 = factor(self.c2,0,1)
+        self.assert_(f2==None)
+        print f2
 
+        f4 = factor(self.c4,0,1)
+        self.assert_(f4==None)
+        print f4
         
 if __name__ == '__main__':
     unittest.main()
