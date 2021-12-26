ea8bdb233d9cd73a80c8fd33d21b8bebb5bd2865
thomas-kh-tran <64433615+thomas-kh-tran@users.noreply.github.com>
2021-12-26 21:21:52 +0100

    Update kbo.py
    first test and look into ocb

--- kbo.py
+++ kbo.py
@@ -19,11 +19,6 @@ class CompareResult(enum.Enum):
     to_lesser = 4
 
 
-#def executeKBO(literals):
-#    print("KBO")
-#    if not literals:
-#        return
-
 def kbocomparevars(term_s, term_t):
     if termIsVar(term_t):
         if term_s == term_t:
@@ -147,4 +142,58 @@ def kbovarcompare(term_s, term_t):  # simplify ?!
         return CompareResult.to_equal
 
 
+def ocbfuncomparepos(ocb, f1, f2):
+    #Compare position of both func to ocb sig_size
+    pass
 
+
+def ocbfuncompare(ocb, f1, f2):
+    ocbsigsize = 0             #in ocb struct
+    if f1 == f2:
+        return CompareResult.to_equal
+    # if f1 == TrueCode:                #truecode on ocb struct
+    #   return CompareResult.to_lesser
+    # if f2 == TrueCode:
+    #   return CompareResult.to_greater
+    if f1 <= ocbsigsize:
+        if f2 <= ocbsigsize:
+            return ocbfuncomparepos(ocb, f1, f2)
+        return CompareResult.to_greater
+    if f2 <= ocbsigsize:
+        return CompareResult.to_lesser
+
+    assert ((f1 > ocbsigsize) and (f1 > ocbsigsize))
+    res = f2 -f2
+    if res < 0:
+        return CompareResult.to_lesser
+    elif res > 0:
+        return  CompareResult.to_greater
+    else:
+        return  CompareResult.to_equal
+
+
+class TestKBO(unittest.TestCase):
+    """
+    Test basic kbo functions.
+    """
+    def setUp(self):
+        self.example1 = "X"
+        self.example2 = "Y"
+        self.example3 = "g(X, f(b))"
+        self.t1 = string2Term(self.example1)
+        self.t2 = string2Term(self.example2)
+        self.t3 = string2Term(self.example3)
+
+    def testkbocomparevars(self):
+        """
+             Test if the kbovarcompare() function work as expected.
+        """
+        self.assertTrue(kbocomparevars(self.t1, self.t1) == CompareResult.to_equal)
+        self.assertTrue(kbocomparevars(self.t1, self.t3) == CompareResult.to_lesser)
+        self.assertTrue(kbocomparevars(self.t3, self.t1) == CompareResult.to_greater)
+        self.assertTrue(kbocomparevars(self.t1, self.t2) == CompareResult.to_greater)
+        self.assertTrue(kbocomparevars(self.t2, self.t3) == CompareResult.to_uncomparable)
+
+
+if __name__ == '__main__':
+    unittest.main()
