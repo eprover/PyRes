d4e6771c8475258c4dfab681fb9eec1352965cb4
Jannis Gehring <jannis-gehring@t-online.de>
2025-04-14 21:40:36 +0200

    chore: rewrite some functions

--- alternatingpath_set.py
+++ alternatingpath_set.py
@@ -67,44 +67,34 @@ class SetRelevanceGraph(RelevanceGraph):
 
     @staticmethod
     def nodes_to_clauses(nodes):
-        clauses = ClauseSet()
-        for node in nodes:
-            clauses.addClause(node.clause)
-        clauses.clauses = set(clauses.clauses)
-        return clauses
+        return { node.clause for node in nodes }
 
     def clauses_to_nodes(self, clauses: ClauseSet):
-        allNodes = self.get_all_nodes()
-        nodesOfClauseSubset = {
-            node for node in allNodes
+        return {
+            node for node in self.get_all_nodes()
             if node.clause in clauses.clauses
         }
-        return nodesOfClauseSubset
 
     @staticmethod
     def edge_neighb_of_subset(edge: Edge, subset: set[Node]):
         return (edge.node1 in subset) != (edge.node2 in subset)
 
     def get_neighbours(self, subset: set[Node]):
-        neighbouring_nodes = set()
         neighbouring_edges = {
             edge for edge in self.edges
             if self.edge_neighb_of_subset(edge, subset)
         }
-
-        for edge in neighbouring_edges:
-            if edge.node1 in subset:
-                neighbouring_nodes.add(edge.node2)
-            else:
-                neighbouring_nodes.add(edge.node1)
+        neighbouring_nodes = {
+            edge.node2 if edge.node1 in subset else edge.node1
+            for edge in neighbouring_edges
+        }
         return neighbouring_nodes
 
     def get_rel_neighbourhood(self, from_clauses: ClauseSet, distance: int):
 
         neighbourhood = self.clauses_to_nodes(from_clauses)
         for _ in range(2 * distance - 1):
-            new_neighbours = self.get_neighbours(neighbourhood)
-            neighbourhood |= new_neighbours
+            neighbourhood |= self.get_neighbours(neighbourhood)
 
         clauses = self.nodes_to_clauses(neighbourhood)
         return clauses
